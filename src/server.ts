832588b15fd0b8f833faaa4c0b25186fedcbc344
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2024-01-15 23:12:33 -0300

    feat: modify message not user

--- src/server.ts
+++ src/server.ts
@@ -1,15 +1,58 @@
 import { fastify } from "fastify";
 import cors from '@fastify/cors'
+import { PrismaClient } from '@prisma/client';
 
 const server = fastify()
 
-server.register(cors, { 
+server.register(cors, {
     origin: '*'
-  })
+})
+
+const prisma = new PrismaClient({
+    log: ['query']
+})
 
 server.get('/', (request, reply) => {
-    return reply.send(JSON.stringify({nome: 'Luiz'}))
+    return reply.send(JSON.stringify({ nome: 'Luiz' }))
 })
+
+server.post('/createUserGestao', async (request: any, response: any) => {
+    const body = request.body
+
+    const userExisting = await prisma.user.findUnique({
+        where: { email: body.email }
+    })
+
+    if (userExisting) {
+        return response.code(409).header('Content-Type', 'application/json; charset=utf-8').send({ message: 'User already exists.' })
+    }
+
+    const create = await prisma.user.create({
+        data: {
+            email: body.email,
+            name: body.name,
+            password: body.password,
+        }
+    })
+
+    return response.code(201).header('Content-Type', 'application/json; charset=utf-8').send(create)
+});
+
+server.get('/loginGestao/:username', async (request: any, response: any) => {
+    const username = request.params.username;
+
+    const user = await prisma.user.findUnique({
+        where: { email: username }
+    })
+
+    // response.code(409).header('Content-Type', 'application/json; charset=utf-8').send({ message: 'User not exists.' })
+
+    if (!user) {
+        return;
+    }
+
+    return response.header('Content-Type', 'application/json; charset=utf-8').send(user)
+});
 
 server.listen({
     host: '0.0.0.0',
