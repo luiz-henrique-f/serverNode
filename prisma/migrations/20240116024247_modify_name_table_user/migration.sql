ea40d7054c25f23e311245c57bd338ba8ab96f1a
Luiz Henrique Fernandes da Silva <luizhfernandes100@gmail.com>
2024-01-15 23:43:20 -0300

    fix: modify name table user

---
+++ prisma/migrations/20240116024247_modify_name_table_user/migration.sql
@@ -0,0 +1,23 @@
+/*
+  Warnings:
+
+  - You are about to drop the `users` table. If the table is not empty, all the data it contains will be lost.
+
+*/
+-- DropTable
+DROP TABLE "users";
+
+-- CreateTable
+CREATE TABLE "user" (
+    "id" TEXT NOT NULL,
+    "name" TEXT,
+    "email" TEXT,
+    "password" TEXT NOT NULL,
+    "emailVerified" TIMESTAMP(3),
+    "image" TEXT,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "user_email_key" ON "user"("email");
